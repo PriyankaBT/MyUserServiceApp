b7b5ada5f92de7109af466191035d7c9f1660b49
PRIYANKA TANDEL <priyankamurkute25@gmail.com>
2020-05-13 00:54:10 +0530

    module1 UPDATE completed

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -13,9 +13,9 @@ export class UserListComponent implements OnInit {
 
   ngOnInit(): void {
     this.userList = [
-      {id: 1, firstName :'Priyanka', lastName :'Murkute',email :'AA', profession:'SS',dob: new Date(1986, 11, 25), imageUrl:'assets/IMG1.jpeg'},
-      {id: 2, firstName :'Jayesh2', lastName :'Murkute',email :'AA', profession:'SS',dob: new Date(1987, 3, 1), imageUrl:'assets/IMG2.jpeg'},
-      {id: 3, firstName :'Jayesh3', lastName :'Murkute',email :'AA', profession:'SS',dob:new Date(1988, 3, 15), imageUrl:'assets/IMG1.jpeg'}    
+      {id: 1, firstName :'Priyanka', lastName :'Murkute',email :'abc@yahoo.com', profession:'Trainer',dob: new Date(1986, 11, 25), imageUrl:'assets/IMG1.jpeg'},
+      {id: 2, firstName :'Jayesh', lastName :'Murkute',email :'aa@gmail.com', profession:'Chartered Accountant',dob: new Date(1987, 3, 1), imageUrl:'assets/IMG2.jpeg'},
+      {id: 3, firstName :'Bhupendra', lastName :'Murkute',email :'xyz@gmail.com', profession:'SOFTWARE eNGINEER',dob:new Date(1988, 3, 15), imageUrl:'assets/IMG1.jpeg'}    
     ]
   }
 
